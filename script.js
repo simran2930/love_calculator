42d365679402dabb91d20f9ab2cfbf43a3de42bd
simran2930 <simranjitkaur.31.2005@gmail.com>
2024-09-26 15:00:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function calculateLove()
+{
+    let name1 = document.getElementById('name1').value;
+    let name2 = document.getElementById('name2').value;
+    
+    if(name1!=""&& name2!="")
+    {
+        let percentage = Math.random() * 100;
+        let lovePercentage = Math.floor(percentage);
+        document.getElementById("lovepercent").textContent = `${lovePercentage}%!`;
+    }
+    else
+    {
+        document.getElementById("lovepercentage").textContent="Please enter both names!";
+    }
+}
+ 
